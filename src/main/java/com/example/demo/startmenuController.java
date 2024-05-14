d666636483f7bb6ca52bd2d9dd4f674e73bf75bd
georgios <minasssidis.g21@gmail.com>
2024-05-15 00:28:58 +0200

    halloi

--- src/main/java/com/example/demo/startmenuController.java
+++ src/main/java/com/example/demo/startmenuController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -16,17 +15,6 @@ public class startmenuController {
     @FXML
     private void wechselZuSpielerauswahl(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("spielerauswahl.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
-    }
-
-
-    @FXML
-    private void wechselZuStartmenu(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("startmenu.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
