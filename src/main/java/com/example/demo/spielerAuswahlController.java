c5536decf3573df61928865663074871841c1cf5
phlippepple <Wi23120@lehre.dhbw-stuttgart.de>
2024-05-15 08:27:39 +0200

    ff

--- src/main/java/com/example/demo/spielerAuswahlController.java
+++ src/main/java/com/example/demo/spielerAuswahlController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import highscoreTabelle.HSTabelle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import spielerPackage.Computer_Spieler;
 import spielerPackage.Echter_Spieler;
@@ -32,7 +35,25 @@ public class spielerAuswahlController {
     private Button spielStarten;
 
     @FXML
-    private void wechselZuSpielfeld(ActionEvent event) throws IOException {
+    private void wechselZuSpielfeldklick(ActionEvent event) throws IOException, InterruptedException {
+        wechselZuSpielfeld(event);
+    }
+
+
+    private void wechselZuSpielfeld(KeyEvent event) throws IOException, InterruptedException {
+        Hauptspiel.spielStarten();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("spielfeld.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        spielfeldController controller = loader.getController();
+        controller.spielStartVorbereiten();
+        stage.setResizable(false);
+        stage.show();
+    }
+    private void wechselZuSpielfeld(ActionEvent event) throws IOException, InterruptedException {
         Hauptspiel.spielStarten();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("spielfeld.fxml"));
@@ -58,7 +79,7 @@ public class spielerAuswahlController {
     }
 
     @FXML
-    private void spielerHinzufuegen(ActionEvent event) throws IOException {
+    private void spielerHinzufuegenklick(ActionEvent event) throws IOException, InterruptedException {
 
             if (nameTextField.getText().isEmpty() || nameTextField.getText().length() > 15 || checkDuplikate.contains(nameTextField.getText()) || nameTextField.getText().contains(";")){
                 error.setVisible(true);
@@ -87,5 +108,37 @@ public class spielerAuswahlController {
             if (Hauptspiel.getAnzahlSpieler() == 4) {
                 wechselZuSpielfeld(event);
             }
+    }
+    @FXML
+    private void hinzufuegenEnter(KeyEvent event) throws IOException, InterruptedException {
+        if (event.getCode()== KeyCode.ENTER){
+            if (nameTextField.getText().isEmpty() || nameTextField.getText().length() > 15 || checkDuplikate.contains(nameTextField.getText()) || nameTextField.getText().contains(";")){
+                error.setVisible(true);
+                error.setText("Der Name ist ungültig.");
+                return;
+            }
+            error.setVisible(false);
+            checkDuplikate.add(nameTextField.getText());
+
+            if (computerCheck.isSelected()) {
+                Spieler computerSpieler = new Computer_Spieler();
+                computerSpieler.setName(nameTextField.getText());
+                Hauptspiel.spielerHinzufuegen(computerSpieler);
+                nameTextField.clear();
+            }
+            else {
+                Spieler spieler = new Echter_Spieler(nameTextField.getText());
+                Hauptspiel.spielerHinzufuegen(spieler);
+                nameTextField.clear();
+            }
+
+            if (Hauptspiel.getAnzahlSpieler() >= 2) {
+                spielStarten.setDisable(false);
+            }
+
+            if (Hauptspiel.getAnzahlSpieler() == 4) {
+                wechselZuSpielfeld(event);
+            }
+        }
     }
 }
