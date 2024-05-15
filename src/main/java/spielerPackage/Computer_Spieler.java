ab70038198afa4b5912073de638a76d96ca5c8ff
FrankSxhneider <wi23180@lehre.dhbw-stuttgart.de>
2024-05-15 13:24:18 +0200

    update algorithm

--- src/main/java/spielerPackage/Computer_Spieler.java
+++ src/main/java/spielerPackage/Computer_Spieler.java
@@ -1,6 +1,8 @@
 package spielerPackage;
+
 import spielerPackage.algorithmusBaum.AlgBaumKnoten;
 import spiellogikPackage.Hauptspiel;
+
 import static spielerPackage.algorithmusBaum.AlgBaum.*;
 
 import java.util.Random;
@@ -8,12 +10,12 @@ import java.util.Random;
 public class Computer_Spieler extends Spieler {
 
     public Computer_Spieler(String name) {
-        this.setName(name + "(Computer)");
+        this.setName(name + "(KI)");
     }
 
     public static int zugMachen_1vs1() {
         int neueZahl = 9;
-        if (Hauptspiel.getAnzahlSpieler()>2 && Hauptspiel.getStack().getLast() + Hauptspiel.getStack().get(Hauptspiel.getAnzahlSpieler()-2) >=21) {
+        if (Hauptspiel.getStack().size() >= 2 && (Hauptspiel.getStack().getLast() + Hauptspiel.getStack().get(Hauptspiel.getStack().size() - 2) >= 21)) {
             neueZahl = 0;
         } else {
             switch (Hauptspiel.getStack().size()) {
@@ -21,7 +23,7 @@ public class Computer_Spieler extends Spieler {
                     neueZahl = 1;
                     break;
                 case 1, 3, 5:
-                    while (Hauptspiel.getStack().getLast() + neueZahl >=21){ // möglichst große Zahl addieren
+                    while (Hauptspiel.getStack().getLast() + neueZahl >= 21) { // möglichst große Zahl addieren
                         neueZahl--;
                     }
                     break;
@@ -37,20 +39,18 @@ public class Computer_Spieler extends Spieler {
         int aktuelleRundenanzahl = Hauptspiel.getRundenAnzahl();
         int anzahlSpieler = Hauptspiel.getAnzahlSpieler();
         int neueZahl;
-        if (aktuelleRundenanzahl <4) {
-            neueZahl = (new Random()).nextInt(9) +1;
+        if (aktuelleRundenanzahl < 4) {
+            neueZahl = (new Random()).nextInt(9) + 1;
         }
-
-        if (Hauptspiel.getStack().getLast() + Hauptspiel.getStack().get(Hauptspiel.getAnzahlSpieler()-2) >=21) {
+        if (Hauptspiel.getStack().size() >= 2 && (Hauptspiel.getStack().getLast() + Hauptspiel.getStack().get(Hauptspiel.getStack().size() - 2) >= 21)) {
             neueZahl = 0;
-        }
-        else if (Hauptspiel.getStack().size()==6) {
-            neueZahl=0;
-        }
-        else {
+        } else if (Hauptspiel.getStack().size() == 6) {
+            neueZahl = 0;
+        } else {
             int zuege = anzahlSpieler * 2;
             AlgBaumKnoten wurzel = baumErstellen(aktuelleRundenanzahl, zuege);
-            setzeGewinnwahrscheinlichkeiten(wurzel, anzahlSpieler, zuege); ;
+            setzeGewinnwahrscheinlichkeiten(wurzel, anzahlSpieler, zuege);
+            ;
             neueZahl = bestimmeDenNaechstenZug(wurzel);
         }
         return neueZahl;
